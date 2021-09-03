ab52e62a276e80d88a2a8fadcb15278c0fbdf94e
Kris Scott <kscott91@gmail.com>
2021-09-02 21:49:45 -0500

    Remove old debug comments

--- test.sh
+++ test.sh
@@ -12,8 +12,3 @@ cargo test
 mkdir -p test/
 cargo run --bin devtest -- -o test/spec.yml
 cargo run -- -vv test/spec.yml -e test/ApiTypes.elm -r test/api_types.rs -t test/apiTypes.ts
-
-# if this overflows, debug with
-# rustup default stable-gnu
-# cargo build
-# rust-lldb -- target/debug/main.exe (args)
